105b27a3641e124a5575d72c36ba7569a3bc27ec
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-10-14 23:16:22 -0700

    Added preload and basic gamemanager functionality

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -6,13 +7,37 @@ public class GameManager : MonoBehaviour {
 
     [SerializeField]
     private int hitPoint;
+    private bool _isPaused;
 
     private void Awake( ) {
         if( _instance == null ) {
             _instance = this;
-            DontDestroyOnLoad( this );
         } else {
             Destroy( this );
         }
     }
+    private void Start( ) {
+        Screen.SetResolution( 1600, 900, false );
+    }
+    private void Update( ) {
+        if( SceneManager.GetActiveScene( ).name == ( "_preload" ) ) {
+            SceneManager.LoadScene( 1 );
+        }
+        if( Input.GetKeyDown( KeyCode.Q ) ) {
+            Application.Quit( );
+        }
+        if( Input.GetKeyDown( KeyCode.Escape ) ) {
+            SceneManager.LoadScene( 0 );
+        }
+        if( Input.GetKeyDown( KeyCode.P ) ) {
+            if( !_isPaused ) {
+                _isPaused = true;
+                Time.timeScale = 0;
+            } else {
+                _isPaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
 }
