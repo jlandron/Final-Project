0da5d8e9bb3451b722365995f806403ca06d7ca5
Sean Miles <sean.miles@outlook.com>
2019-10-27 21:55:32 -0700

    Added Health System and Weapon Aiming Prototypes

---
+++ Assets/Scripts/PlayerHealthSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealthSystem : MonoBehaviour
+{
+    public Text m_HealthDisplay = null;
+
+    private int m_DefaultHealth = 3;
+    private int m_Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Health = m_DefaultHealth;
+        UpdateUI();
+    }
+
+    public void IncrementHealth()
+    {
+        if (m_Health < m_DefaultHealth)
+        {
+            m_Health++;
+            UpdateUI();
+        }
+    }
+
+    public void DecrementHealth()
+    {
+        if (m_Health > 1)
+        {
+            m_Health--;
+            UpdateUI();
+        }
+        else
+        {
+            SceneManager.LoadScene("HealthSystem");
+        }
+    }
+
+    public void UpdateUI()
+    {
+        m_HealthDisplay.text = "Health: " + m_Health;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            DecrementHealth();
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponControl : MonoBehaviour
+{
+    public Camera m_MainCamera = null;
+    private bool direction = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+        direction = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Code used is from following this tutorial: https://www.youtube.com/watch?v=47xWM1RcY3Y
+
+        // Rotation
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5f;
+
+        Vector3 objectPos = m_MainCamera.WorldToScreenPoint(transform.position);
+
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        if (angle > 0f && angle < 100f || angle < 0f && angle > -90f)
+        {
+            if (direction == false)
+            {
+                direction = true;
+                Flip();
+            }
+        }
+
+        if (angle > 100f && angle < 180f || angle < -90f && angle > -180f)
+        {
+            if (direction == true)
+            {
+                direction = false;
+
+                Flip();
+            }
+        }
+    }
+
+    public void Flip()
+    {
+        Vector3 flipScale = transform.localScale;
+        flipScale.x *= -1;
+        transform.localScale = flipScale;
+
+        Vector3 flipScaleParent = transform.parent.localScale;
+        flipScaleParent.x *= -1;
+        transform.parent.localScale = flipScaleParent;
+    }
+}
