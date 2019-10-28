3c72dbc6a776189e7afce3599ced9085dcb501e0
dhengkt <49250200+dhengkt@users.noreply.github.com>
2019-10-27 20:54:29 -0700

    Implemented Draining Battery and Recharging
    Change prototype5 to DrainingBatteryAndRecharge

---
+++ Assets/Scripts/DrainingBattery.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrainingBattery : MonoBehaviour
+{
+    [SerializeField]
+    private float TotalHp = 100;
+    [SerializeField]
+    public float CurrentHp;
+    
+    void Start()
+    {
+        CurrentHp = TotalHp;
+    }
+
+    void Update()
+    {
+        if (CurrentHp > 0f)
+        {
+            DamageOverTime();
+        }
+        if (CurrentHp < 0f)
+        {
+            CurrentHp = TotalHp;
+        }
+    }
+
+    void DamageOverTime()
+    {
+        CurrentHp -= .05f;
+        transform.localScale = new Vector3((CurrentHp / TotalHp), 1, 1);
+        // add change color later
+    }
+
+}
