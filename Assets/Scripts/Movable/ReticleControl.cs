1dc5931873f316f8003a4276ab83c7f7aa556c35
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-11-04 14:40:12 -0800

    rebuild for WebGL

--- Assets/Scripts/Movable/ReticleControl.cs
+++ Assets/Scripts/Movable/ReticleControl.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class ReticleControl : MonoBehaviour
 {
     private Vector3 mouseCoords;
-    public float MouseSensitivity = 0.1f;
+    public float MouseSensitivity = 0.5f;
+    private Camera _camera;
+
+    private void Start( ) {
+        _camera = FindObjectOfType<Camera>( );
+    }
     void Update()
     {
         mouseCoords = Input.mousePosition;
-        mouseCoords = Camera.main.ScreenToWorldPoint( mouseCoords );
+        mouseCoords = _camera.ScreenToWorldPoint( mouseCoords );
         this.gameObject.transform.position = Vector2.Lerp( transform.position, mouseCoords, MouseSensitivity );
     }
 }
