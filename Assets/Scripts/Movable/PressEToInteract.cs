a0eeb288582560f357b4ada1d5261bb2644bfb0a
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-11-07 18:46:37 -0800

    Made Text stick to interactable

--- Assets/Scripts/Movable/PressEToInteract.cs
+++ Assets/Scripts/Movable/PressEToInteract.cs
@@ -1,42 +1,131 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-namespace Game.Movable {
-    public class PressEToInteract : MonoBehaviour {
+namespace Game.Movable
+{
+    public class PressEToInteract : MonoBehaviour
+    {
         [SerializeField]
-        private Text signal = null;
+        private GameObject signal = null;
         [SerializeField]
         private Text wantToShow = null;
         [SerializeField]
         private bool _inRange = false;
+        private bool showSignal = false;
 
-        private void Start( ) {
-            signal.gameObject.SetActive( false );
-            wantToShow.gameObject.SetActive( false );
+        private MeshRenderer signalMeshRenderer;
+
+        [SerializeField]
+        private float changeTimeSeconds = 1f;
+        [SerializeField]
+        private float startScale = 0;
+        [SerializeField]
+        private float endScale = 0.4f;
+        private float _startScale;
+        private float _endScale;
+        float changeRate = 0;
+        float timeSoFar = 0;
+        bool fading = false;
+
+        private void Start()
+        {
+            signalMeshRenderer = signal.GetComponent<MeshRenderer>();
+            //signal.gameObject.SetActive( false );
+            SetScale(0);
+            wantToShow.gameObject.SetActive(false);
         }
 
-        private void OnTriggerEnter2D( Collider2D collision ) {
-            if( collision.gameObject.tag == "Player" ) {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
                 _inRange = true;
-                signal.gameObject.SetActive( true );
+                //signal.gameObject.SetActive( true );
+                FadeIn();
             }
         }
-        private void Update( ) {
-            if( _inRange && Input.GetKeyDown( KeyCode.E ) ) {
-                Debug.Log( "Showing Text" );
-                wantToShow.gameObject.SetActive( true );
-                Invoke( "ShowText", 5 );
-            }
-        }
-        private void OnTriggerExit2D( Collider2D collision ) {
-            if( collision.gameObject.tag == "Player" ) {
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
                 _inRange = false;
-                signal.gameObject.SetActive( false );
+                //signal.gameObject.SetActive( false );
+                FadeOut();
             }
         }
-        void ShowText( ) {
-            Debug.Log( "Disabling Text" );
-            wantToShow.gameObject.SetActive( false );
+        private void Update()
+        {
+            if (_inRange && Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Showing Text");
+                wantToShow.gameObject.SetActive(true);
+                Invoke("DisableText", 5);
+            }
+            if (showSignal)
+            {
+
+            }
+        }
+
+        void DisableText()
+        {
+            Debug.Log("Disabling Text");
+            wantToShow.gameObject.SetActive(false);
+        }
+
+        public void FadeIn()
+        {
+
+            _startScale = startScale;
+            _endScale = endScale;
+
+            timeSoFar = 0;
+            fading = true;
+            StartCoroutine(FadeCoroutine());
+        }
+
+        public void FadeOut()
+        {
+
+
+            _startScale = endScale;
+            _endScale = startScale;
+
+            timeSoFar = 0;
+            fading = true;
+            StartCoroutine(FadeCoroutine());
+        }
+
+        IEnumerator FadeCoroutine()
+        {
+            changeRate = (_endScale - _startScale) / changeTimeSeconds;
+            SetScale(_startScale);
+            while (fading)
+            {
+                timeSoFar += Time.deltaTime;
+
+                if (timeSoFar > changeTimeSeconds)
+                {
+                    fading = false;
+                    SetScale(_endScale);
+                    yield break;
+                }
+                else
+                {
+
+                    SetScale(signalMeshRenderer.transform.localScale.x + (changeRate * Time.deltaTime));
+                }
+
+                yield return null;
+            }
+        }
+
+
+
+        public void SetScale(float scale)
+        {
+            Vector2 scaleVec = new Vector2(Mathf.Clamp(scale, startScale, endScale), Mathf.Clamp(scale, startScale, endScale));
+            signalMeshRenderer.transform.localScale = scaleVec;
         }
     }
 
---
+++ Assets/Scripts/UI/RenderOnLayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderOnLayer : MonoBehaviour
+{
+    public string layer;
+
+    private void Awake()
+    {
+        GetComponent<MeshRenderer>().sortingLayerName = layer;
+        GetComponent<MeshRenderer>().sortingOrder = 0;
+    }
+}
